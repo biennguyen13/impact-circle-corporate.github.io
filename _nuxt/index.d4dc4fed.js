8a59c1915240168bcddbb97039cd44fe82ca6a0a
biennguyen <biennguyen131311@gmail.com>
2023-10-21 17:13:57 +0700

    test

---
+++ _nuxt/index.d4dc4fed.js
@@ -0,0 +1,1 @@
+import{d as n,r as s,o,c as a,u as c}from"./entry.2af8bd1c.js";const u=n({__name:"index",setup(r){const{$axios:t}=c();return t.get("news").then(e=>e),s({search:"",options:null}),(e,p)=>(o(),a("div"))}});export{u as default};
